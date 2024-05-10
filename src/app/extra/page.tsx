6f7096982429fdbb32f5436e876e2a4bf483911f
Calvin Coderes <calvin.c.r.coderes@gmail.com>
2024-05-10 12:21:47 +0800

    moved assets to public folder; Did set up the fonts, and tested a bit.  this should get more cohesive on the succeeding commits

---
+++ src/app/extra/page.tsx
@@ -0,0 +1,5 @@
+export default function Exra() {
+    return(
+        <h1>Extra page</h1>
+    )
+}
---
+++ src/styles/utils.tsx
@@ -0,0 +1,9 @@
+
+export const spacing = {
+    headerDefault: '185px',
+}
+
+export const colors = {
+    mainBackground: '#0E1414',
+    mainFont: '#FFFFFF'
+}
